3efb6615272254d6d47b3bad5e87288fdc9ef3ea
David Wickes <dave.wickes@gmail.com>
2019-08-15 22:39:21 +0100

    day three part two

--- 2015/go/day-three/day-three.go
+++ 2015/go/day-three/day-three.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/gypsydave5/advent-of-code/2015/go/tools/by"
 )
 
 func main() {
+	partTwo()
+}
+
+func partOne() {
 	var x, y int
 	var grid = make(map[int]map[int]int)
 	grid[x] = make(map[int]int)
@@ -28,6 +33,48 @@ func main() {
 		}
 		grid[x][y]++
 	})
+
+	var result int
+
+	for _, xs := range grid {
+		for range xs {
+			result++
+		}
+	}
+
+	fmt.Printf("Houses Delivered To: %d\n", result)
+}
+
+func partTwo() {
+	var sx, sy, rx, ry int
+	var grid = make(map[int]map[int]int)
+	grid[sx] = make(map[int]int)
+	grid[sx][sy] += 2
+
+	s, _ := ioutil.ReadFile("day-three-input.txt")
+	for i, r := range s {
+		var x, y *int
+		if (i % 2) == 0 {
+			x, y = &sx, &sy
+		} else {
+			x, y = &rx, &ry
+		}
+		switch r {
+		case '^':
+			*y++
+		case 'v':
+			*y--
+		case '>':
+			*x++
+		case '<':
+			*x--
+		}
+		_, present := grid[*x]
+		if !present {
+			grid[*x] = make(map[int]int)
+		}
+		grid[*x][*y]++
+	}
 
 	var result int
 
