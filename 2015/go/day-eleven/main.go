bd2cdd2e5696dd43568571c5a3000324b9440cd0
David Wickes <dave.wickes@gmail.com>
2019-09-06 17:57:54 +0100

    day eleven; incrementing a string working

--- 2015/go/day-eleven/main.go
+++ 2015/go/day-eleven/main.go
@@ -2,12 +2,26 @@ package main
 
 func increment(s string) string {
 	bs := []byte(s)
-	for i := len(bs) - 1; i >= 0; i-- {
-		if bs[i] < 'z' {
-			bs[i]++
-			return string(bs)
-		}
+	b := innercrement(bs)
+	if b {
+		return string(append(bs, 'a'))
 	}
 
 	return string(bs)
+}
+
+func innercrement(bs []byte) bool {
+	for i := len(bs) - 1; i >= 0; i-- {
+		if bs[i] < 'z' {
+			bs[i]++
+			return false
+		}
+		if bs[i] == 'z' {
+			bs[i] = 'a'
+			if i == 0 {
+				return true
+			}
+		}
+	}
+	return false
 }
--- 2015/go/day-eleven/main_test.go
+++ 2015/go/day-eleven/main_test.go
@@ -12,10 +12,14 @@ func TestIncrementString(t *testing.T) {
 	}{
 		{"a", "b"},
 		{"aa", "ab"},
+		{"az", "ba"},
+		{"z", "aa"},
+		{"zzz", "aaaa"},
+		{"zaz", "zba"},
 	}
 
 	for _, c := range cases {
-		t.Run(fmt.Sprintf("%s -> %s\n", c.input, c.expected), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%s->%s", c.input, c.expected), func(t *testing.T) {
 			actual := increment(c.input)
 			if actual != c.expected {
 				t.Errorf("Expected %q but got %q", c.expected, actual)
