70ff1266ed08bd84d02fc86dd12e22fea986ffd9
David Wickes <dave.wickes@gmail.com>
2019-09-11 22:01:47 +0100

    day eleven

--- 2015/go/day-eleven/main.go
+++ 2015/go/day-eleven/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
+func main() {
+	current := "cqjxjnds"
+	current = "cqjxxyzz"
+	for {
+		current = increment(current)
+		if validPassword(current) {
+			break
+		}
+	}
+
+	fmt.Println(current)
+}
+
+func validPassword(s string) bool {
+	return hasTwoPair(s) && hasThreeStraight(s) && hasNoForbiddenLetters(s)
+}
+
 func increment(s string) string {
 	bs := []byte(s)
 	b := innercrement(bs)
@@ -24,4 +43,45 @@ func innercrement(bs []byte) bool {
 		}
 	}
 	return false
+}
+
+func hasThreeStraight(s string) bool {
+	if len(s) < 3 {
+		return false
+	}
+	for i := 2; i < len(s); i++ {
+		if s[i-2] == s[i]-2 && s[i-1] == s[i]-1 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasTwoPair(s string) bool {
+	if len(s) < 4 {
+		return false
+	}
+
+	pairCount := 0
+
+	for i := 1; i < len(s); i++ {
+		if s[i-1] == s[i] {
+			pairCount++
+			i++
+		}
+		if pairCount == 2 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasNoForbiddenLetters(s string) bool {
+	for i := range s {
+		switch s[i] {
+		case 'i', 'o', 'l':
+			return false
+		}
+	}
+	return true
 }
--- 2015/go/day-eleven/main_test.go
+++ 2015/go/day-eleven/main_test.go
@@ -27,3 +27,39 @@ func TestIncrementString(t *testing.T) {
 		})
 	}
 }
+
+func TestHasThreeStraight(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected bool
+	}{
+		{"hijklmmn", true},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%q->%v", c.input, c.expected), func(t *testing.T) {
+			actual := hasThreeStraight(c.input)
+			if actual != c.expected {
+				t.Errorf("Expected %v but got %v", c.expected, actual)
+			}
+		})
+	}
+}
+
+func TestHasTwoPair(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected bool
+	}{
+		{"abbceffg", true},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%q->%v", c.input, c.expected), func(t *testing.T) {
+			actual := hasTwoPair(c.input)
+			if actual != c.expected {
+				t.Errorf("Expected %v but got %v", c.expected, actual)
+			}
+		})
+	}
+}
